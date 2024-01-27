19b9644276b47f2f65fb31441d6a2ab089bb0a96
myreage <remy.vandyk@gmail.com>
2024-01-27 19:46:43 +0100

    fix persistence locaiton

--- src/persistence.rs
+++ src/persistence.rs
@@ -1,6 +1,8 @@
 use std::{
-    fs::File,
+    env,
+    fs::{self, File},
     io::{self, Write},
+    path::{Path, PathBuf},
 };
 
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
@@ -13,7 +15,7 @@ struct FileData {
     pub current_stack: String,
 }
 
-fn write_to_file<T: Serialize>(data: &T, file_path: &str) -> io::Result<()> {
+fn write_to_file<T: Serialize>(data: &T, file_path: &PathBuf) -> io::Result<()> {
     let file = File::create(file_path)?;
     let mut writer = io::BufWriter::new(file);
     serde_json::to_writer_pretty(&mut writer, data)?;
@@ -21,7 +23,7 @@ fn write_to_file<T: Serialize>(data: &T, file_path: &str) -> io::Result<()> {
     Ok(())
 }
 
-fn read_from_file<T: DeserializeOwned + Default + Serialize>(file_path: &str) -> io::Result<T> {
+fn read_from_file<T: DeserializeOwned + Default + Serialize>(file_path: &PathBuf) -> io::Result<T> {
     let file = match File::open(file_path) {
         Ok(file) => file,
         Err(_) => {
@@ -34,10 +36,11 @@ fn read_from_file<T: DeserializeOwned + Default + Serialize>(file_path: &str) ->
     Ok(serde_json::from_reader(reader)?)
 }
 
-pub fn read_current_stack_branches() -> Result<Vec<Branch>, &'static str> {
-    let file_data = match read_from_file::<FileData>("save.json") {
+pub fn read_current_stack_branches() -> Result<Vec<Branch>, String> {
+    let save_file_path = get_save_file_path()?;
+    let file_data = match read_from_file::<FileData>(&save_file_path) {
         Ok(loaded_stacks) => loaded_stacks,
-        Err(_) => return Err("Error when reading file"),
+        Err(e) => return Err(format!("Error reading file {}", e)),
     };
 
     let current_stack = &file_data.current_stack;
@@ -53,7 +56,8 @@ pub fn read_current_stack_branches() -> Result<Vec<Branch>, &'static str> {
 }
 
 pub fn save_stack(stack_name: &String) -> Result<(), String> {
-    let mut file_data = match read_from_file::<FileData>("save.json") {
+    let save_file_path = get_save_file_path()?;
+    let mut file_data = match read_from_file::<FileData>(&save_file_path) {
         Ok(loaded_stacks) => loaded_stacks,
         Err(_) => return Err("Error when reading file".to_string()),
     };
@@ -64,7 +68,7 @@ pub fn save_stack(stack_name: &String) -> Result<(), String> {
 
     file_data.stacks.push(Stack::new(stack_name));
 
-    match write_to_file(&file_data, "save.json") {
+    match write_to_file(&file_data, &save_file_path) {
         Ok(_) => {
             eprintln!("Created new stack {}", stack_name);
             Ok(())
@@ -74,7 +78,8 @@ pub fn save_stack(stack_name: &String) -> Result<(), String> {
 }
 
 pub fn save_branch_on_current_stack(branch_name: &String) -> Result<(), String> {
-    let mut file_data = match read_from_file::<FileData>("save.json") {
+    let save_file_path = get_save_file_path()?;
+    let mut file_data = match read_from_file::<FileData>(&save_file_path) {
         Ok(loaded_stacks) => loaded_stacks,
         Err(_) => return Err("Error when reading file".to_string()),
     };
@@ -100,7 +105,7 @@ pub fn save_branch_on_current_stack(branch_name: &String) -> Result<(), String> 
 
     current_stack.branches.push(Branch::new(branch_name));
 
-    match write_to_file(&file_data, "save.json") {
+    match write_to_file(&file_data, &save_file_path) {
         Ok(_) => {
             eprintln!("Created new branch {}", branch_name);
             Ok(())
@@ -110,7 +115,8 @@ pub fn save_branch_on_current_stack(branch_name: &String) -> Result<(), String> 
 }
 
 pub fn save_current_stack(stack_name: &String) -> Result<(), String> {
-    let mut file_data = match read_from_file::<FileData>("save.json") {
+    let save_file_path = get_save_file_path()?;
+    let mut file_data = match read_from_file::<FileData>(&save_file_path) {
         Ok(loaded_stacks) => loaded_stacks,
         Err(_) => return Err("Error when reading file".to_string()),
     };
@@ -122,7 +128,7 @@ pub fn save_current_stack(stack_name: &String) -> Result<(), String> {
 
     file_data.current_stack = stack_name.clone();
 
-    match write_to_file(&file_data, "save.json") {
+    match write_to_file(&file_data, &save_file_path) {
         Ok(_) => {
             eprintln!("On stack {}", stack_name);
             Ok(())
@@ -132,9 +138,11 @@ pub fn save_current_stack(stack_name: &String) -> Result<(), String> {
 }
 
 pub fn read_current_stack() -> Result<Stack, String> {
-    let file_data = match read_from_file::<FileData>("save.json") {
+    let save_file_path = get_save_file_path()?;
+    dbg!(&save_file_path);
+    let file_data = match read_from_file::<FileData>(&save_file_path) {
         Ok(loaded_stacks) => loaded_stacks,
-        Err(_) => return Err("Error when reading file".to_string()),
+        Err(e) => return Err(e.to_string()),
     };
 
     let current_stack = match file_data
@@ -147,4 +155,43 @@ pub fn read_current_stack() -> Result<Stack, String> {
     };
 
     Ok(current_stack.clone())
+}
+
+fn find_git_parent_dir() -> Option<PathBuf> {
+    let mut current_dir = env::current_dir().ok()?;
+
+    let git_dir = current_dir.join(".git");
+
+    if git_dir.is_dir() {
+        return Some(current_dir);
+    }
+
+    while current_dir.pop() {
+        let git_dir = current_dir.join(".git");
+
+        if git_dir.is_dir() {
+            return Some(current_dir);
+        }
+    }
+
+    None
+}
+
+fn create_stacky_folder_and_save_json(parent_dir: &Path) -> Result<PathBuf, String> {
+    let stacky_dir = parent_dir.join(".stacky");
+    let save_json_path = stacky_dir.join("save.json");
+
+    if !stacky_dir.is_dir() {
+        fs::create_dir_all(&stacky_dir).map_err(|e| e.to_string())?;
+    }
+
+    Ok(save_json_path)
+}
+
+fn get_save_file_path() -> Result<PathBuf, String> {
+    if let Some(git_parent_dir) = find_git_parent_dir() {
+        create_stacky_folder_and_save_json(&git_parent_dir)
+    } else {
+        Err("Aucun dossier '.git' trouvé dans l'arborescence.".to_string())
+    }
 }
